bb2df6368d98eb6d4425ee144cf206bd452de4c6
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-03-02 16:25:00 +0800

    use const instead of var

--- src/lib/Frequency.svelte
+++ src/lib/Frequency.svelte
@@ -5,41 +5,41 @@
 	//@ts-ignore
 	import data from '$lib/frequency.csv';
 
-	var parseDate = d3.timeParse('%Y/%m/%d');
+	const parseDate = d3.timeParse('%Y/%m/%d');
 
 	export let height = 250;
 	export let width = 350;
-	var maxDate = d3.max(data, function (d) {
+	const maxDate = d3.max(data, function (d) {
 		return parseDate(d.Date);
 	});
-	var minDate = d3.min(data, function (d) {
+	const minDate = d3.min(data, function (d) {
 		return parseDate(d.Date);
 	});
-	var maxNum = d3.max(data, function (d) {
+	const maxNum = d3.max(data, function (d) {
 		return Number(d.num);
 	});
 
 	onMount(() => {
-		var parseDate = d3.timeParse('%Y/%m/%d');
+		const parseDate = d3.timeParse('%Y/%m/%d');
 
-		var y = d3
+		const y = d3
 			.scaleLinear()
 			.domain([0, maxNum])
 			.range([height - 70, 0]);
 
-		var x = d3
+		const x = d3
 			.scaleTime()
 			.domain([minDate, maxDate])
 			.range([0, width - 70]);
 
-		var yAxis = d3.axisLeft(y);
-		var xAxis = d3.axisBottom(x).ticks(5);
+		const yAxis = d3.axisLeft(y);
+		const xAxis = d3.axisBottom(x).ticks(5);
 
-		var svg = d3.select(svg_node).attr('height', height).attr('width', width);
+		const svg = d3.select(svg_node).attr('height', height).attr('width', width);
 
-		var chartGroup = svg.append('g').attr('transform', 'translate(40, 30)');
+		const chartGroup = svg.append('g').attr('transform', 'translate(40, 30)');
 
-		var line = d3
+		const line = d3
 			.line()
 			.x(function (d) {
 				return x(parseDate(d.Date));
